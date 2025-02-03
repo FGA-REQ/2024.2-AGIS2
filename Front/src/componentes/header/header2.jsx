37623c93406dd3c1e98a55a364562f74a55b2bb3
Oleari19 <mariaclaraoleari@gmail.com>
2025-02-03 12:02:33 -0300

    feat: botão de logout no header e mudanças no style

--- Front/src/componentes/header/header2.jsx
+++ Front/src/componentes/header/header2.jsx
@@ -1,25 +1,41 @@
-// Header depois do Login
-
-import React from 'react';
+import React, { useState } from 'react';
 import './header2.css';
 
 function Header2({ toggleSidebar }) {
+    const [menuAberto, setMenuAberto] = useState(false);
+
+    const handleMenuToggle = () => {
+        setMenuAberto(!menuAberto);
+    };
+
+    const handleLogout = () => {
+        navigate("/");
+    };
+
     return (
         <header className="header2">
-            {/* Botão hambúrguer */}
+
             <div className="menu-icon" onClick={toggleSidebar}>
                 ☰ {/* Ícone de hambúrguer */}
             </div>
 
-            {/* Centralização da logo e título */}
             <div className="conteudo-header-admin">
                 <img className="logo-header-admin" src="/logo.svg" alt="Logo do Med Manager" />
                 <h1>Med Manager</h1>
             </div>
 
-            {/* Informações do usuário */}
             <div className="infos-header2">
-                <img src="user.svg" alt="Ícone de usuário" className="user-icon-medicos" />
+                <img
+                    src="user.svg"
+                    alt="Ícone de usuário"
+                    className="user-icon-header2"
+                    onClick={handleMenuToggle}
+                />
+                {menuAberto && (
+                    <div className="menu-dropdown">
+                        <button onClick={handleLogout}>Log out</button>
+                    </div>
+                )}
             </div>
         </header>
     );
