d43357461bb456b2745906397f11ec73aeeb51ff
ailujana <anajumesantos11@hotmail.com>
2025-02-08 16:41:11 -0300

    feat: rota de update e remove dos agendamentos

--- schedule-service/src/drugschedule/drugschedule.service.ts
+++ schedule-service/src/drugschedule/drugschedule.service.ts
@@ -41,11 +41,50 @@ export class DrugscheduleService {
     return `This action returns a #${id} drugschedule`;
   }
 
-  update(id: number, updateDrugscheduleDto: UpdateDrugscheduleDto) {
-    return `This action updates a #${id} drugschedule`;
+  async update(id: number, updateDrugscheduleDto: UpdateDrugscheduleDto) {
+    try {
+      const drugSchedule = await this.prisma.drugSchedule.findUnique({ where: { id } });
+      if (!drugSchedule) throw new BadRequestException(`O agendamento de remédio com ID ${id} não foi encontrado.`)
+
+      const { drugId, numberOfDays, initialDate, drugBreak, patientId } = updateDrugscheduleDto;
+
+      if (drugId) {
+        const isThereADrug = await this.apiService.get(`http://localhost:3000/patient/${drugId}`);
+        if (!isThereADrug) throw new BadRequestException(`O remédio informado não existe`);
+      }
+
+      if (patientId) {
+        const isThereAPatient = await this.apiService.get(`http://localhost:3000/patient/${patientId}`);
+        if (!isThereAPatient) throw new BadRequestException(`O paciente informado não existe`);
+      }
+
+      if (patientId && drugId && initialDate) {
+        const checkDrugSchedule = await this.prisma.drugSchedule.findFirst({
+          where: { patientId, drugId, initialDate, NOT: { id } }
+        });
+        if (checkDrugSchedule) throw new BadRequestException(`Já existe um agendamento para este paciente e medicamento neste horário.`);
+      }
+
+      const updatedSchedule = await this.prisma.drugSchedule.update({ where: { id }, data: updateDrugscheduleDto });
+      this.logger.log(`Agendamento com ID ${id} atualizado com sucesso.`);
+      return updatedSchedule;
+    } catch (error) {
+      this.logger.error(`Falha ao atualizar agendamento ${id}: ${error.message}`);
+      throw error;
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} drugschedule`;
+  async remove(id: number) {
+    try {
+      const drugSchedule = await this.prisma.drugSchedule.findUnique({ where: { id } });
+      if (!drugSchedule) throw new BadRequestException(`O agendamento de remédio com ID ${id} não foi encontrado.`);
+
+      await this.prisma.drugSchedule.delete({ where: { id } });
+      this.logger.log(`agendamento de remédio com ID ${id} removido com sucesso.`);
+    } catch (error) {
+      this.logger.error(`Falha ao remover agendamento de remédio ${id}: ${error.message}`);
+      throw error;
+    }
   }
+
 }
