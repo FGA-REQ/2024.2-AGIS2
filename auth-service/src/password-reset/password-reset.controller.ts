87a65c2f954143200e2030ccf35cfcd1c481c40b
junioramaral22 <junior.amaral.1818@gmail.com>
2025-02-09 22:32:08 -0300

    Fix: iniciado fix recuperação de senha

--- auth-service/src/password-reset/password-reset.controller.ts
+++ auth-service/src/password-reset/password-reset.controller.ts
@@ -8,17 +8,15 @@ export class PasswordResetController {
   @Post('send-token')
   async sendResetToken(
     @Body('email') email: string,
-    @Body('userType') userType: "doctor" | "patient",
   ) {
-    return await this.passwordResetService.sendResetToken(email, userType);
+    return await this.passwordResetService.sendResetToken(email);
   }
   @Post('reset-password')
   async resetPassword(
     @Body('email') email: string,
     @Body('token') token: string,
     @Body('newPassword') newPassword: string,
-    @Body('userType') userType: "doctor" | "patient",
   ){
-    return await this.passwordResetService.resetPassword(email, token, newPassword, userType);
+    return await this.passwordResetService.resetPassword(email, token, newPassword);
   }
 }
